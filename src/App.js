e53ede0f4f6f0c7b89f984ad478ec021e9445aef
atidele <alejandro.tidele@mercadolibre.com>
2021-07-15 11:59:44 -0300

    Starting with the challenge

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      <h2 className="tomato">React + Webpack 5</h2>
+      <h3 className="tomato-blue">Date : {new Date().toDateString()}</h3>
+    </div>
+  );
+}
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+  output: {
+    path: path.join(__dirname, "/dist"),
+    filename: "index.bundle.js",
+  },
+  devServer: {
+    port: 3000,
+    watchContentBase: true,
+  },
+  // Rules of how webpack will take our files, compile & bundle them for the browser
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /nodeModules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+      {
+        test: /\.(scss|css)$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({ template: "./public/index.html" }),
+    new MiniCssExtractPlugin(),
+  ],
+};
